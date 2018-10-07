7b966df54df90720d73a584bdd3adb012174fafb
unknown <nairakshay77@gmail.com>
2018-10-07 17:25:39 +0530

    added additional files for execution

--- main.py
+++ main.py
@@ -5,6 +5,11 @@ app = Flask(__name__)
 def get_my_ip():
     return "hello world"
 
+@app.route("/", methods=["GET"])
+def homepage():
+    return "<html><body>Hello world</body></html>"
+
+
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port=port)
